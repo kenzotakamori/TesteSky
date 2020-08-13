c5ff8aacdd6402a234740acc048f1b3200f2e5fd
kenzotakamori <marceloktakamori@gmail.com>
2020-08-13 15:04:06 -0300

    CSS adjustments / Get Request

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
     $("header").load("header.html")
     $("footer").load("footer.html")
+    var url = 'https://sky-frontend.herokuapp.com/movies'
+    $.get(url, function(data){
+        console.log('data', data);
+    })
 });
